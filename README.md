d6e7776d2a3cd3f0586188b265ee2d8f58bbeaf4
park-cheol <ekqqqus5555@gamil.com>
2021-08-13 15:54:44 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+## DCCRN: Deep Complex Convolution Recurrent Network
+* Paper:  https://arxiv.org/abs/2008.00264
+* Official Code: https://github.com/huyanxin/DeepComplexCRN
+
+**- Network**
+![arch](https://user-images.githubusercontent.com/76771847/129316005-1c19ff9a-dd37-43e6-982e-0b1dd298acc9.png)
+>DCUNet 와 비슷한 형태로 다른 점은 가운데 Bridge 부분에 Complex LSTM이 있다.
+또한 STFT을 Conv1d, ISTFT을 ConvTranspose1d 을 사용하였다.
+
+**- Module**
+![module](https://user-images.githubusercontent.com/76771847/129316229-51ca7c51-c058-40f4-a1f5-80d47e32a236.png)
+> Real과 Imag 을 Stack(or Concat)으로 뭉쳐서 Conv(or Batch, ConvTranspose) 실행 할 때 다시 chunk(or Slicing)으로 풀어 실행
+> 
+> Complex Multiplication 식과 동일하게 수행
+
+**- Loss**
+
+![loss](https://user-images.githubusercontent.com/76771847/129316595-bad11735-78e6-45e1-8fa5-fe67e422423f.png)
+
+## Reference
+
+code: https://github.com/stdKonjac
+
+code: https://github.com/seorim0/DCCRN-with-various-loss-functions
+
+DCUnet code
+
---
+++ model/complex_nn.py
@@ -0,0 +1,425 @@
+"""
+official code
+https://github.com/huyanxin/DeepComplexCRN/blob/bc6fd38b0af9e8feb716c81ff8fbacd7f71ad82f/complexnn.py#L79
+"""
+
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def get_casual_padding1d():
+    pass
+
+
+def get_casual_padding2d():
+    pass
+
+
+def complex_cat(inputs, axis):
+    real, imag = [], []
+    for idx, data in enumerate(inputs):
+        r, i = torch.chunk(data, 2, axis)
+        real.append(r)
+        imag.append(i)
+    real = torch.cat(real, axis)
+    imag = torch.cat(imag, axis)
+    outputs = torch.cat([real, imag], axis)
+    return outputs
+
+
+"""
+DCUnet 은 [batch, channel, H, W, 2 = real + imag] 인 반면에
+DCCRN 은 [batch, real+imag, ...] 식으로 진행되는 것 같음
+"""
+# negative slope 가 hyperparam 인 LeakyReLU 와 다르게 slope 를 param으로 두어 학습 하게 함
+class cPReLU(nn.Module):
+
+    def __init__(self, complex_axis=1):
+        super(cPReLU, self).__init__()
+        self.r_prelu = nn.PReLU()
+        self.i_prelu = nn.PReLU()
+        self.complex_axis = complex_axis
+
+    def forward(self, inputs):
+        real, imag = torch.chunk(inputs, 2, self.complex_axis)
+        real = self.r_prelu(real)
+        imag = self.i_prelu(imag)
+        return torch.cat([real, imag], self.complex_axis)
+
+
+class cLSTM(nn.Module):
+
+    def __init__(self, input_size, hidden_size, projection_dim=None, bidirectional=False, batch_first=False):
+        super(cLSTM, self).__init__()
+        # todo(check): Complex LSTM 이므로 2번의 LSTM이 필요해서 computation 을 줄이기 위해 2로 나눠주는 것인지
+        self.input_dim = input_size // 2
+        self.rnn_units = hidden_size // 2
+        self.real_lstm = nn.LSTM(self.input_dim, self.rnn_units, num_layers=1, bidirectional=bidirectional,
+                                 batch_first=False)
+        self.imag_lstm = nn.LSTM(self.input_dim, self.rnn_units, num_layers=1, bidirectional=bidirectional,
+                                 batch_first=False)
+
+        if bidirectional:
+            bidirectional = 2
+        else:
+            bidirectional = 1
+        if projection_dim is not None:
+            self.projection_dim = projection_dim // 2
+            self.r_trans = nn.Linear(self.rnn_units * bidirectional, self.projection_dim)
+            self.i_trans = nn.Linear(self.rnn_units * bidirectional, self.projection_dim)
+        else:
+            self.projection_dim = None
+
+    def forward(self, inputs):
+        if isinstance(inputs, list): # input 이 List 형식일 경우
+            real, imag = inputs
+
+        elif isinstance(inputs, torch.Tensor):
+            real, imag = torch.chunk(inputs, -1)
+            # todo torch.chunk(inputs, chunks=2, dim=-1) 실행 후 확인
+
+        # todo [0]
+        r2r_out = self.real_lstm(real)[0] # real + real lstm
+        r2i_out = self.imag_lstm(real)[0] # real + imag lstm
+        i2r_out = self.real_lstm(imag)[0] # imag + real lstm
+        i2i_out = self.imag_lstm(imag)[0] # imag + imag lstm
+
+        # Complex multiplication
+        real_out = r2r_out - i2i_out
+        imag_out = i2r_out + r2i_out
+
+        if self.projection_dim is not None:
+            real_out = self.r_trans(real_out)
+            imag_out = self.i_trans(imag_out)
+
+        output = [real_out, imag_out] # List 형식으로 반환
+
+        return output
+
+    def flatten_parameters(self): # todo RNN 종류를 실행 시키기전에 항상 하는것 무엇인지 파악
+        self.imag_lstm.flatten_parameters()
+        self.real_lstm.flatten_parameters()
+
+
+class cConv2d(nn.Module):
+
+    def __init__(
+            self,
+            in_channels,
+            out_channels,
+            kernel_size=(1, 1),
+            stride=(1, 1),
+            padding=(0, 0),
+            dilation=1,
+            groups=1,
+            causal=True,
+            complex_axis=1):
+        """
+        in_channels: real + imag
+        out_channels: real + imag
+        causal: if causal, will padding time dimension's left side,
+                    otherwise both(인과 시스템)
+        """
+        super(cConv2d, self).__init__()
+
+        self.in_channels = in_channels // 2
+        self.out_channels = out_channels // 2
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.causal = causal
+        self.groups = groups
+        self.dilation = dilation
+        self.complex_axis = complex_axis
+
+        # todo padding[x, y] 있는데 x는 freq padding , y는 time padding 인 것 같다
+        self.real_conv = nn.Conv2d(self.in_channels, self.out_channels, kernel_size, self.stride,
+                                   padding=[self.padding[0], 0], dilation=self.dilation, groups=self.groups)
+
+        self.imag_conv = nn.Conv2d(self.in_channels, self.out_channels, kernel_size, self.stride,
+                                   padding=[self.padding[0], 0], dilation=self.dilation, groups=self.groups)
+
+        nn.init.normal_(self.real_conv.weight.data, std=0.05)
+        nn.init.normal_(self.imag_conv.weight.data, std=0.05)
+        nn.init.constant_(self.real_conv.bias, 0.)
+        nn.init.constant_(self.imag_conv.bias, 0.)
+
+    def forward(self, inputs):
+        if self.padding[1] != 0 and self.causal: # todo time padding 이 있고 인과시스템일 경우
+            inputs = F.pad(inputs, [self.padding[1], 0, 0, 0])
+            # F.pad(left, right, top, bottom)
+        else:
+            inputs = F.pad(inputs, [self.padding[1], self.padding[0], 0, 0])
+
+        if self.complex_axis == 0:
+            # todo list 형식인지 확인
+            real = self.real_conv(inputs)
+            imag = self.imag_conv(inputs)
+            # todo [real + imag]식으로 있는 듯
+            real2real, imag2real = torch.chunk(real, 2, self.complex_axis)
+            real2imag, imag2imag = torch.chunk(imag, 2, self.complex_axis)
+        else:
+            if isinstance(inputs, torch.Tensor):
+                real, imag = torch.chunk(inputs, 2, self.complex_axis)
+
+            real2real = self.real_conv(real, )
+            imag2imag = self.imag_conv(imag, )
+
+            real2imag = self.imag_conv(real)
+            imag2real = self.real_conv(imag)
+
+        real = real2real - imag2imag
+        imag = real2imag + imag2real
+        out = torch.cat([real, imag], self.complex_axis)
+
+        return out
+
+
+class cConvTranspose2d(nn.Module):
+
+    def __init__(
+            self,
+            in_channels,
+            out_channels,
+            kernel_size=(1,1),
+            stride=(1,1),
+            padding=(0,0),
+            output_padding=(0,0),
+            causal=False,
+            complex_axis=1,
+            groups=1):
+        super(cConvTranspose2d, self).__init__()
+
+        self.in_channels = in_channels // 2
+        self.out_channels = out_channels // 2
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.output_padding = output_padding
+        self.groups = groups
+        self.complex_axis=complex_axis
+
+        self.real_conv = nn.ConvTranspose2d(self.in_channels, self.out_channels, kernel_size, self.stride,
+                                            padding=self.padding, output_padding=output_padding, groups=self.groups)
+        self.imag_conv = nn.ConvTranspose2d(self.in_channels, self.out_channels, kernel_size, self.stride,
+                                            padding=self.padding, output_padding=output_padding, groups=self.groups)
+
+        nn.init.normal_(self.real_conv.weight, std=0.05)
+        nn.init.normal_(self.imag_conv.weight, std=0.05)
+        nn.init.constant_(self.real_conv.bias, 0.)
+        nn.init.constant_(self.imag_conv.bias, 0.)
+
+    def forward(self, inputs):
+        if isinstance(inputs, torch.Tensor):
+            real, imag = torch.chunk(inputs, 2, self.complex_axis)
+
+        elif isinstance(inputs, tuple) or isinstance(inputs, list):
+            real = inputs[0]
+            imag = inputs[1]
+
+        if self.complex_axis == 0:
+            real = self.real_conv(inputs)
+            imag = self.imag_conv(inputs)
+            real2real, imag2real = torch.chunk(real, 2, self.complex_axis)
+            real2imag, imag2imag = torch.chunk(imag, 2, self.complex_axis)
+
+        else:
+            if isinstance(inputs, torch.Tensor):
+                real, imag = torch.chunk(inputs, 2, self.complex_axis)
+
+            real2real = self.real_conv(real, )
+            imag2imag = self.imag_conv(imag, )
+
+            real2imag = self.imag_conv(real)
+            imag2real = self.real_conv(imag)
+
+        real = real2real - imag2imag
+        imag = real2imag + imag2real
+        out = torch.cat([real, imag], self.complex_axis)
+
+        return out
+
+
+class cBatchNorm2d(nn.Module):
+    def __init__(self, num_features, eps=1e-5, momentum=0.1, affine=True,
+                 track_running_stats=True, complex_axis=1):
+        super(cBatchNorm2d, self).__init__()
+
+        self.num_features = num_features // 2
+        self.eps = eps
+        self.momentum = momentum
+        self.affine = affine
+        self.track_running_stats = track_running_stats
+        self.complex_axis = complex_axis
+
+        if self.affine:
+            self.Wrr = torch.nn.Parameter(torch.Tensor(self.num_features))
+            self.Wri = torch.nn.Parameter(torch.Tensor(self.num_features))
+            self.Wii = torch.nn.Parameter(torch.Tensor(self.num_features))
+            self.Br = torch.nn.Parameter(torch.Tensor(self.num_features))
+            self.Bi = torch.nn.Parameter(torch.Tensor(self.num_features))
+        else:
+            self.register_parameter('Wrr', None)
+            self.register_parameter('Wri', None)
+            self.register_parameter('Wii', None)
+            self.register_parameter('Br', None)
+            self.register_parameter('Bi', None)
+
+        if self.track_running_stats:
+            self.register_buffer('RMr', torch.zeros(self.num_features))
+            self.register_buffer('RMi', torch.zeros(self.num_features))
+            self.register_buffer('RVrr', torch.ones(self.num_features))
+            self.register_buffer('RVri', torch.zeros(self.num_features))
+            self.register_buffer('RVii', torch.ones(self.num_features))
+            self.register_buffer('num_batches_tracked', torch.tensor(0, dtype=torch.long))
+        else:
+            self.register_parameter('RMr', None)
+            self.register_parameter('RMi', None)
+            self.register_parameter('RVrr', None)
+            self.register_parameter('RVri', None)
+            self.register_parameter('RVii', None)
+            self.register_parameter('num_batches_tracked', None)
+        self.reset_parameters()
+
+        def reset_running_stats(self):
+            if self.track_running_stats:
+                self.RMr.zero_()
+                self.RMi.zero_()
+                self.RVrr.fill_(1)
+                self.RVri.zero_()
+                self.RVii.fill_(1)
+                self.num_batches_tracked.zero_()
+
+        def reset_parameters(self):
+            self.reset_running_stats()
+            if self.affine:
+                self.Br.data.zero_()
+                self.Bi.data.zero_()
+                self.Wrr.data.fill_(1)
+                self.Wri.data.uniform_(-.9, +.9)  # W will be positive-definite
+                self.Wii.data.fill_(1)
+
+        def _check_input_dim(self, xr, xi):
+            assert (xr.shape == xi.shape)
+            assert (xr.size(1) == self.num_features)
+
+        def forward(self, inputs):
+            # self._check_input_dim(xr, xi)
+
+            xr, xi = torch.chunk(inputs, 2, axis=self.complex_axis)
+            exponential_average_factor = 0.0
+
+            if self.training and self.track_running_stats:
+                self.num_batches_tracked += 1
+                if self.momentum is None:  # use cumulative moving average
+                    exponential_average_factor = 1.0 / self.num_batches_tracked.item()
+                else:  # use exponential moving average
+                    exponential_average_factor = self.momentum
+
+            #
+            # NOTE: The precise meaning of the "training flag" is:
+            #       True:  Normalize using batch   statistics, update running statistics
+            #              if they are being collected.
+            #       False: Normalize using running statistics, ignore batch   statistics.
+            #
+            training = self.training or not self.track_running_stats
+            redux = [i for i in reversed(range(xr.dim())) if i != 1]
+            vdim = [1] * xr.dim()
+            vdim[1] = xr.size(1)
+
+            #
+            # Mean M Computation and Centering
+            #
+            # Includes running mean update if training and running.
+            #
+            if training:
+                Mr, Mi = xr, xi
+                for d in redux:
+                    Mr = Mr.mean(d, keepdim=True)
+                    Mi = Mi.mean(d, keepdim=True)
+                if self.track_running_stats:
+                    self.RMr.lerp_(Mr.squeeze(), exponential_average_factor)
+                    self.RMi.lerp_(Mi.squeeze(), exponential_average_factor)
+            else:
+                Mr = self.RMr.view(vdim)
+                Mi = self.RMi.view(vdim)
+            xr, xi = xr - Mr, xi - Mi
+
+            #
+            # Variance Matrix V Computation
+            #
+            # Includes epsilon numerical stabilizer/Tikhonov regularizer.
+            # Includes running variance update if training and running.
+            #
+            if training:
+                Vrr = xr * xr
+                Vri = xr * xi
+                Vii = xi * xi
+                for d in redux:
+                    Vrr = Vrr.mean(d, keepdim=True)
+                    Vri = Vri.mean(d, keepdim=True)
+                    Vii = Vii.mean(d, keepdim=True)
+                if self.track_running_stats:
+                    self.RVrr.lerp_(Vrr.squeeze(), exponential_average_factor)
+                    self.RVri.lerp_(Vri.squeeze(), exponential_average_factor)
+                    self.RVii.lerp_(Vii.squeeze(), exponential_average_factor)
+            else:
+                Vrr = self.RVrr.view(vdim)
+                Vri = self.RVri.view(vdim)
+                Vii = self.RVii.view(vdim)
+            Vrr = Vrr + self.eps
+            Vri = Vri
+            Vii = Vii + self.eps
+
+            #
+            # Matrix Inverse Square Root U = V^-0.5
+            #
+            # sqrt of a 2x2 matrix,
+            # - https://en.wikipedia.org/wiki/Square_root_of_a_2_by_2_matrix
+            tau = Vrr + Vii
+            delta = torch.addcmul(Vrr * Vii, -1, Vri, Vri)
+            s = delta.sqrt()
+            t = (tau + 2 * s).sqrt()
+
+            # matrix inverse, http://mathworld.wolfram.com/MatrixInverse.html
+            rst = (s * t).reciprocal()
+            Urr = (s + Vii) * rst
+            Uii = (s + Vrr) * rst
+            Uri = (- Vri) * rst
+
+            #
+            # Optionally left-multiply U by affine weights W to produce combined
+            # weights Z, left-multiply the inputs by Z, then optionally bias them.
+            #
+            # y = Zx + B
+            # y = WUx + B
+            # y = [Wrr Wri][Urr Uri] [xr] + [Br]
+            #     [Wir Wii][Uir Uii] [xi]   [Bi]
+            #
+            if self.affine:
+                Wrr, Wri, Wii = self.Wrr.view(vdim), self.Wri.view(vdim), self.Wii.view(vdim)
+                Zrr = (Wrr * Urr) + (Wri * Uri)
+                Zri = (Wrr * Uri) + (Wri * Uii)
+                Zir = (Wri * Urr) + (Wii * Uri)
+                Zii = (Wri * Uri) + (Wii * Uii)
+            else:
+                Zrr, Zri, Zir, Zii = Urr, Uri, Uri, Uii
+
+            yr = (Zrr * xr) + (Zri * xi)
+            yi = (Zir * xr) + (Zii * xi)
+
+            if self.affine:
+                yr = yr + self.Br.view(vdim)
+                yi = yi + self.Bi.view(vdim)
+
+            outputs = torch.cat([yr, yi], self.complex_axis)
+            return outputs
+
+        def extra_repr(self):
+            return '{num_features}, eps={eps}, momentum={momentum}, affine={affine}, ' \
+                   'track_running_stats={track_running_stats}'.format(**self.__dict__)
+
+
